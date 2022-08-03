c0731c5abdda5ea03be03cf5a16b5da81390428c
Ogilboot <callum_ogilvy@hotmail.co.uk>
2022-08-03 12:06:38 +0000

    feat: added checkMatch and flipCard functions

--- app.js
+++ app.js
@@ -52,13 +52,36 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random())
 
 const gridDisplay = document.querySelector('#grid')
+const cardsChosen = []
+const cardsChosenIds = []
 
-function createBoard () {
+function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
         const card = document.createElement('img')
         card.setAttribute('src', 'images/blank.png')
         card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
         gridDisplay.append(card)
     }
 }
 createBoard()
+
+function checkMatch() {
+    const cards = document.querySelectorAll('#grid img')
+    console.log('Check for match!')
+    if (cardsChosen[0] == cardsChosen[1]) {
+        alert('You found a match!')
+        cards[cardsChosenIds[0]].setAttribute('src', 'images/white.png')
+        cards[cardsChosenIds[1]].setAttribute('src', 'images/white.png')
+    }
+}
+
+function flipCard() {
+    const cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenIds.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length === 2) {
+        setTimeout(checkMatch, 500)
+    }
+}
