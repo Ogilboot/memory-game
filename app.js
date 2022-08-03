e81b8ac39352997d8fec55cce2c9f76bd90b0a75
Ogilboot <callum_ogilvy@hotmail.co.uk>
2022-08-03 11:40:08 +0000

    feat: added createBoard func & styling

--- app.js
+++ app.js
@@ -53,3 +53,12 @@ cardArray.sort(() => 0.5 - Math.random())
 
 const gridDisplay = document.querySelector('#grid')
 
+function createBoard () {
+    for (let i = 0; i < cardArray.length; i++) {
+        const card = document.createElement('img')
+        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('data-id', i)
+        gridDisplay.append(card)
+    }
+}
+createBoard()
